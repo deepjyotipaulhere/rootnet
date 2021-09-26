909d297fc8186bb8ccc414107104322d1c4bd48c
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2021-09-26 07:39:54 +0530

    update settings

--- myAxios.js
+++ myAxios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const myAxios=axios.create({
     baseURL: 'https://api.fitrockr.com',
     headers:{
-        'X-API-Key': '141d352f-4bd8-48e8-88c0-d505b1ec0236',
+        'X-API-Key': 'xxxx',
         'X-Tenant': 'hackzurich'
     }
 })
